bf46bf4b346d4a9be56762f2fe8ba65b599ddd65
DM Naufal <dmnaufal7@gmail.com>
2021-02-16 13:43:27 +0700

    add php file

---
+++ flutter_crud/employee_actions.php
@@ -0,0 +1,98 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "flutter_crud";
+    $table = "Employees";
+
+    //Action from app to do operations in DB.
+    $action = $_POST["action"];
+
+    //Create connection.
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Check connection.
+    if($conn->connect_error){
+        die("Connection Failed: " . $conn->connect_error);
+        return;
+    }
+
+    //If connection is OK
+
+    //If the app sends an action to create the table
+    if("CREATE_TABLE" == $action){
+        $sql = "CREATE TABLE IF NOT EXISTS $table (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            first_name VARCHAR(30) NOT NULL,
+            last_name VARCHAR(30) NOT NULL
+        )";
+
+        if($conn->query($sql) === TRUE){
+            //Send back success message.
+            echo "success";
+        }else{
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }
+
+    //Get all employee records from the database
+    if("GET_ALL" == $action){
+        $db_data = array();
+        $sql = "SELECT id, first_name, last_name from $table ORDER BY id DESC";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                $db_data[] = $row;
+            }
+            //Send back the complete records as JSON
+            echo json_encode($db_data);
+        }else{
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }
+
+    //Add employee
+    if("ADD_EMP" == $action){
+        $first_name = $_POST["first_name"];
+        $last_name = $_POST["last_name"];
+        $sql = "INSERT INTO $table (first_name, last_name) VALUES ('$first_name', '$last_name')";
+        $result = $conn->query($sql);
+        echo "success";
+        $conn->close();
+        return;
+    }
+
+    //Update employee
+    if("UPDATE_EMP" == $action){
+        //App post value to server
+        $emp_id = $_POST['emp_id'];
+        $first_name = $_POST["first_name"];
+        $last_name = $_POST["last_name"];
+        $sql = "UPDATE $table SET first_name = '$first_name', last_name = '$last_name' WHERE id = $emp_id";
+        if($conn->query($sql) === TRUE){
+            echo "success";
+        }else{
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }
+
+    //Delete employee
+    if("DELETE_EMP" == $action){
+        $emp_id = $_POST['emp_id'];
+        $sql = "DELETE FROM $table WHERE id = $emp_id";
+        if($conn->query($sql) === TRUE){
+            echo "success";
+        }else{
+            echo "error";
+        }
+        $conn->close();
+        return;
+    }
+
+?>
